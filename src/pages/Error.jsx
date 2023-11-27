ba32ec75e7fab0da352b8c14f1e184c43eb5282f
Alyssa <alyssaroque01@gmail.com>
2023-11-26 20:25:47 -0800

    add astronaut cat image

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,20 @@
+import { useRouteError } from "react-router-dom"
+import GirlFancy1 from "../assets/GirlFancy1.png";
+import AstronautCat from "../assets/AstronautCat1.png";
+
 const Error = () => { 
+    const error = useRouteError()
+
     return (
-        <div>Error</div>
+        <div className="error">
+            <h1>Houston we have a problem...</h1>
+            <img src={AstronautCat} width={600}></img>
+            <p>{error.message || error.statusText}</p>
+            Error    
+        </div>
     )
 }
 
 export default Error
+// astronaut cat picture is personal use only!!
+//confused cat picture is personal use only!!
